be49b6c840cb7a3632a37d962d058496a731901d
Bar Oren <bar.oren10@gmail.com>
2023-07-29 14:42:17 +0300

    kinda working db

--- src/main/java/hac/DebugController.java
+++ src/main/java/hac/DebugController.java
@@ -1,7 +1,5 @@
 package hac;
 
-import hac.repo.Purchase;
-import hac.repo.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
